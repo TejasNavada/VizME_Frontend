97c3cc7a5f2edd47fa7ad21cc449ad0eead03215
TejasNavada <tejasnavada2002@gmail.com>
2025-01-02 12:43:04 -0500

    ssl

--- src/tool/socketIO.js
+++ src/tool/socketIO.js
@@ -1,5 +1,11 @@
 const io = require('socket.io-client')
-export let socket = io(process.env.REACT_APP_SOCKET_HOST);
+export let socket = io(process.env.REACT_APP_SOCKET_HOST,{
+    secure: true,  
+    transports: ["websocket", "polling"], // Ensure both transports are used
+    reconnection: true, // Enable automatic reconnection
+    reconnectionAttempts: 5, // Retry up to 5 times
+    reconnectionDelay: 1000, // Retry after 1 second
+});
 
 socket.on("connect", () => {
     console.log("Connected to server:", socket.id);
